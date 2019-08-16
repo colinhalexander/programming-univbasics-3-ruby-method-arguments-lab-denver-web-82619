c236958a7200fa0fba6459c1cd8b4060545752eb
Colin Alexander <chalexander@me.com>
2019-08-16 20:54:02 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -7,11 +7,11 @@ def introduction(name)
   puts "Hi, my name is #{name}"
 end
 
-introduction_with_language(name, language)
+def introduction_with_language(name, language)
   "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-introduction_with_language_optional(name, language = 'Ruby')
+def introduction_with_language_optional(name, language = 'Ruby')
   "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
